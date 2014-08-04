6d1239e8b6e409121e9f03030535bcda3e7b6716
zumbalogy <intrapostal@gmail.com>
2014-08-03 20:42:11 -0400

    koans done

--- src/koans/21_group_by.clj
+++ src/koans/21_group_by.clj
@@ -2,13 +2,13 @@
   (:require [koan-engine.core :refer :all]))
 
 (defn get-odds-and-evens [coll]
-  (let [{odds true evens false} (group-by __ coll)]
+  (let [{odds true evens false} (group-by odd? coll)]
     [odds evens]))
 
 (meditations
 
   "To categorize a collection by some function, use group-by."
-  (= __ (group-by count ["hello" "world" "foo" "bar"]))
+  (= {5 ["hello" "world"], 3 ["foo" "bar"]} (group-by count ["hello" "world" "foo" "bar"]))
 
 
   "You can simulate filter + remove in one pass"
@@ -17,7 +17,7 @@
      [[1 3 5] [2 4]])
 
   "You can also group by a primary key"
-  (= __
+  (= {1 [{:id 1 :name "Bob"}  {:id 1 :last-name "Smith"}] 2 [{:id 2 :name "Mike"}]}
      (group-by :id [{:id 1 :name "Bob"}
                     {:id 2 :name "Mike"}
                     {:id 1 :last-name "Smith"} ]))
@@ -25,13 +25,13 @@
   "But be careful when you group by non-required key"
   (= {"Bob" [{:name "Bob" :id 1}]
       "Mike" [{:name "Mike" :id 2}]
-      __ [{:last-name "Smith" :id 1}]}
+      nil [{:last-name "Smith" :id 1}]}
    (group-by :name [{:id 1 :name "Bob"}
                     {:id 2 :name "Mike"}
                     {:id 1 :last-name "Smith"}]))
 
   "The true power of group-by comes with custom functions"
-  (= __
+  (= {:nice-list [{:name "Jack" :bad false}] :naughty-list [{:name "Jimmy" :bad true} {:name "Joe" :bad true}]}
      (group-by #(if (:bad %) :naughty-list :nice-list)
                [{:name "Jimmy" :bad true}
                 {:name "Jack" :bad false}
